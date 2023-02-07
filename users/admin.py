5d376e07b697307c400873f8ffde99d216d76476
LevonAS <LevonAS@example.com>
2023-02-07 23:13:09 +0300

    lesson_1 done

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import CustomUser as CustomUser
+
+admin.site.register(CustomUser, UserAdmin)
+
+# @admin.register(users_models.CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ['username', 'first_name','last_name','email']
---
+++ users/management/commands/create_base_users.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand
+
+from users.models import CustomUser
+
+
+class Command(BaseCommand):
+    help = "Создание базовых пользователей"
+
+    def handle(self, *args, **options):
+        # Создание суперпользователя
+        # User.objects.create_superuser(username='admin', email='admin@totonotes.ru', password='masteradmin')
+        # CustomUser.objects.create_superuser(username='admin', email='admin@totonotes.ru', password='masteradmin')
+        CustomUser.objects.create(
+            username="admin",
+            email="admin@totonotes.ru",
+            password=make_password("masteradmin"),
+            is_superuser=True,
+            is_staff=True,
+        )
+
+        # Сощдание пользователей
+        CustomUser.objects.create(username="user001", first_name="001", last_name="u", email="user001@totonotes.ru")
+        CustomUser.objects.create(username="user002", first_name="002", last_name="u", email="user002@totonotes.ru")
+        CustomUser.objects.create(username="user003", first_name="003", last_name="u", email="user003@totonotes.ru")
+        CustomUser.objects.create(username="user004", first_name="004", last_name="u", email="user004@totonotes.ru")
+        CustomUser.objects.create(username="user005", first_name="005", last_name="u", email="user005@totonotes.ru")
+        # user_add = CustomUser(username='user001', first_name='u', last_name='n', email='user2001@totonotes.ru')
+        # user_add.save()
+
+        self.stdout.write(f"Пользователи созданы")
