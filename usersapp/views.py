78ba4771419e1207a6ea0461faafa2c7d487eea3
LevonAS <LevonAS@example.com>
2023-02-14 17:11:18 +0300

    lesson_4 done

--- usersapp/views.py
+++ usersapp/views.py
@@ -2,12 +2,26 @@
 # Create your views here.
 
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.viewsets import GenericViewSet
 
 from .models import CustomUser
 from .serializers import CustomUserModelSerializer
 
 
-class CustomUserModelViewSet(ModelViewSet):
+class CustomUserLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 4
+
+
+# class CustomUserModelViewSet(ModelViewSet):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = CustomUserModelSerializer
+#     pagination_class = CustomUserLimitOffsetPagination
+
+
+class CustomUserModelViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, GenericViewSet):
+    # renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserModelSerializer
+    pagination_class = CustomUserLimitOffsetPagination
